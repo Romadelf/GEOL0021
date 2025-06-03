9d6c06681a59c7a02d03e33dc439627359d9e097
Romadelf <romadelfatwork@hotmail.com>
2025-06-03 13:47:00 +0200

    FIX : np.logspace expects base 10 by default, but np.log yields base e (ln) -> fix with np.log10 instead

--- ERT_IP/analyse.py
+++ ERT_IP/analyse.py
@@ -42,8 +42,8 @@ print(data.describe())
 
 # 1) Résistance:
 binsR = np.logspace(
-    start=np.log(min(data[labelERT])),
-    stop=np.log(np.quantile(data[labelERT], 0.9)),
+    start=np.log10(min(data[labelERT])),
+    stop=np.log10(np.quantile(data[labelERT], 0.9)),
     num=numberOfBarsInHistogram + 1)
 hist1 = data.hist(
     column=labelERT,
