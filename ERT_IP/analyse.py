0fb9fb59f304d8c57253df7465cb9fccd609347a
Romadelf <romadelfatwork@hotmail.com>
2025-06-03 10:18:07 +0200

    typos

--- ERT_IP/analyse.py
+++ ERT_IP/analyse.py
@@ -10,7 +10,7 @@ import numpy as np
 from matplotlib import pyplot
 # Importer le jeu de données via pandas:
 #   Si le jeux de données contient ERT et IP et que le jeu contient des estimations d'erreur,
-#   les paramètres a entrée sont ceux utilisé ci-dessous.
+#   les paramètres a entrer sont ceux utilisés ci-dessous.
 data = pd.read_csv('./B52_DDN6_essai.ohm', 
                    delimiter='\t', header=None, skiprows=36, 
     names= ['a','b','m','n','R (Ohm)','Res. Error (Ohm)','IP (mV/V)','IP Error (mV/V)'], index_col=False,
